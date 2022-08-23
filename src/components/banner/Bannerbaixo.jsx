4c976f4a7d3f58cb81c6f3cc60798213a1c07e72
Gustavo Vieira <gustavovieirazu@gmail.com>
2022-08-23 12:19:53 -0300

    ajustes

---
+++ src/components/banner/Bannerbaixo.jsx
@@ -0,0 +1,7 @@
+import './banner.css'
+function Bannerbaixo() 
+{
+  return <div class="imgbanner2"></div>
+}
+
+export default Bannerbaixo;
---
+++ src/components/banner/Bannercima.jsx
@@ -0,0 +1,11 @@
+import './banner.css'
+function Bannercima() 
+{
+  return <div class="aa"> 
+  <a class="bannercima" href="/Bannercima.jsx">
+    <div class="imgbanner1"></div>
+  </a>
+  </div>
+}
+
+export default Bannercima;
