a65f238fe36646b7adc3932e04acf4cc67272484
Gustavo Vieira <gustavovieirazu@gmail.com>
2022-08-23 13:38:09 -0300

    ajustes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Bannercima from "./components/banner/Bannercima";
+import Footer from "./components/footer/footer";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <Bannercima />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/Data/dataProduct.js
@@ -0,0 +1,29 @@
+export const Produtos = [
+    {
+        id: 1,
+        name: "Camisa Sol",
+        price: 50,
+        image: "https://images.unsplash.com/photo-1660816610637-14a1a8be79e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=740&q=80",
+
+        qty: 0,
+        total: 0,
+    },
+    {
+        id: 2,
+        name: "vestido estrelas",
+        price: 100,
+        image: "https://images.unsplash.com/photo-1660816610637-14a1a8be79e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=740&q=80",
+
+        qty: 0,
+        total: 0,
+    },
+    {
+        id: 3,
+        name: "camisa sol&lua",
+        price: 65,
+        image: "https://images.unsplash.com/photo-1660816610637-14a1a8be79e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=740&q=80",
+
+        qty: 0,
+        total: 0,
+    },
+];
