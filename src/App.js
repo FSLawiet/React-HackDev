b1cd91ef9bc96de67f2468fbf1acfd6e4786278d
Fausto André <fslawliet@gmail.com>
2022-08-24 09:11:14 -0300

    Finaliação Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import Footer from "./components/Footer";
-
+import "./App.css";
 function App() {
   return (
     <>
-      <h1>Ready to code!</h1>
       <Footer />
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,60 @@
 import React from "react";
 import "./Footer.css";
+import { IconContext } from "react-icons";
+import { FiFacebook, FiInstagram, FiLinkedin, FiYoutube } from "react-icons/fi";
 
 const Footer = () => {
   return (
     <footer>
-      <p>Exemplo de Footer</p>
+      <div id="footer_menu">
+        <ul>
+          <li>
+            <a href="#">Sobre</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+          <li>
+            <a href="#">Ajuda</a>
+          </li>
+          <li>
+            <a href="#">Conta</a>
+          </li>
+        </ul>
+      </div>
+      <div id="footer_sm">
+        <p>Redes Sociais</p>
+        <IconContext.Provider
+          value={{ color: "var(--destaque)", size: "1.6em" }}
+        >
+          <ul>
+            <li>
+              <a href="https://facebook.com/">
+                <FiFacebook />
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/">
+                <FiInstagram />
+              </a>
+            </li>
+            <li>
+              <a href="https://linkedin.com/">
+                <FiLinkedin />
+              </a>
+            </li>
+            <li>
+              <a href="https://youtube.com/">
+                <FiYoutube />
+              </a>
+            </li>
+          </ul>
+        </IconContext.Provider>
+      </div>
+      <div id="footer_info">
+        <p>© Hoshi - CNPJ: 00.000.000/0000-00</p>
+        <p>rua Tal nº 000, Goiânia, Goiás</p>
+      </div>
     </footer>
   );
 };
