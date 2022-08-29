d583bc55be8b23f2af43da63f75ee1b7274f7a5b
Gustavo Vieira <gustavovieirazu@gmail.com>
2022-08-29 12:12:19 -0300

    Versão final Site5

--- src/Data/dataProduct.js
+++ src/Data/dataProduct.js
@@ -1,72 +1,72 @@
 export const Produtos = [
     {
         id: 1,
-        name: "Camisa Sol",
-        price: 50,
+        name: "Camisa estrela",
+        price: 29.9,
         image: "/img/produto1.png",
 
         sizeSelect: "",
     },
     {
         id: 2,
-        name: "vestido estrelas",
-        price: 100,
+        name: "Camisa virgem",
+        price: 69.9,
         image: "/img/produto2.png",
 
         sizeSelect: "",
     },
     {
         id: 3,
-        name: "camisa sol&lua",
-        price: 65,
+        name: "Camisa aquário",
+        price: 59.9,
         image: "/img/produto3.png",
 
         sizeSelect: "",
     },
     {
         id: 4,
-        name: "camisa sol&lua",
-        price: 65,
+        name: "Camisa câncer",
+        price: 49.9,
         image: "/img/produto4.png",
 
         sizeSelect: "",
     },
     {
         id: 5,
-        name: "camisa sol&lua",
-        price: 65,
+        name: "Camisa touro",
+        price: 79.9,
         image: "/img/produto5.png",
 
         sizeSelect: "",
     },
     {
         id: 6,
-        name: "camisa sol&lua",
-        price: 65,
+        name: "Camisa gêmeos",
+        price: 99.9,
         image: "/img/produto6.png",
 
         sizeSelect: "",
     },
     {
         id: 7,
-        name: "camisa sol&lua",
-        price: 65,
+        name: "Camisa leão",
+        price: 59.9,
         image: "/img/produto7.png",
 
         sizeSelect: "",
     },
     {
         id: 8,
-        name: "camisa sol&lua",
-        price: 65,
+        name: "Camisa escorpião",
+        price: 69.9,
         image: "/img/produto8.png",
 
         sizeSelect: "",
     },
     {
         id: 9,
-        name: "camisa sol&lua",
-        price: 65,
+        name: "Camisa sagitário",
+        price: 89.9,
         image: "/img/produto9.png",
 
         sizeSelect: "",
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Card from "../components/card/Card";
 import { Produtos } from "../Data/dataProduct";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
+import Banner2 from "../components/banner/Banner2";
 
 function Home() {
     const pathPage = useNavigate();
@@ -11,13 +12,31 @@ function Home() {
         <>
             <Bannercima />
             <div className='Cards'>
-                {Produtos.map((item, idx) => (
-                    <Card
-                        key={idx}
-                        item={item}
-                        onClick={() => pathPage(`/product/${item.id}`)}
-                    />
-                ))}
+                {Produtos.map((item, idx) => {
+                    if (item.id < 6)
+                        return (
+                            <Card
+                                key={idx}
+                                item={item}
+                                onClick={() => pathPage(`/product/${item.id}`)}
+                            />
+                        );
+                    return null;
+                })}
+            </div>
+            <Banner2 />
+            <div className='Cards'>
+                {Produtos.map((item, idx) => {
+                    if (item.id >= 6)
+                        return (
+                            <Card
+                                key={idx}
+                                item={item}
+                                onClick={() => pathPage(`/product/${item.id}`)}
+                            />
+                        );
+                    return null;
+                })}
             </div>
         </>
     );
