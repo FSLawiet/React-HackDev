c55ba5c2e5f28c8f969f53b2f0d6ae5e154cb8ba
Fausto André <fslawliet@gmail.com>
2022-10-08 08:34:24 -0300

    Alterações Carrinho

--- src/components/itemCarrinho/index.jsx
+++ src/components/itemCarrinho/index.jsx
@@ -3,58 +3,59 @@ import "./style.css";
 import { ProductContext } from "../../context/produtosContext";
 
 function ItemCarrinho({ item }) {
-    const { removeItemCart } = useContext(ProductContext);
+  const { removeItemCart } = useContext(ProductContext);
 
-    const [mult, setMult] = useState(1);
-    const [total, setTotal] = useState(0);
+  const [mult, setMult] = useState(1);
+  const [total, setTotal] = useState(0);
 
-    useEffect(() => {
-        setTotal(mult * item.price);
-    }, [mult, item]);
+  useEffect(() => {
+    setTotal(mult * item.price);
+  }, [mult, item]);
 
-    return (
-        <>
-            <div className='Teste1234'>
-                <div className='Imagem'>
-                    <img src={item.image} alt='img' className='Imagem' />
-                </div>
-                <div className='name'>
-                    <h4>{item.name}:</h4>
-                    <div className='price'>R$ {item.price}</div>
-                    <p>Tamanho: {item.sizeSelect}</p>
-                    <div className='Adicionar-Remover'>
-                        <button
-                            onClick={() => setMult(mult - 1)}
-                            className='Qty__Button'
-                            disabled={mult <= 1}
-                        >
-                            -
-                        </button>
-                        <div className='col-qty'>{mult}</div>
+  return (
+    <>
+      <div className="Teste1234">
+        <div className="Imagem">
+          <img
+            src={"data:image/png;base64," + item.img}
+            alt="img"
+            className="Imagem"
+          />
+        </div>
+        <div className="name">
+          <h4>{item.peca}:</h4>
+          <div className="price">R$ {item.valor}</div>
+          <p>Tamanho: {item.tamanho}</p>
+          <div className="Adicionar-Remover">
+            <button
+              onClick={() => setMult(mult - 1)}
+              className="Qty__Button"
+              disabled={mult <= 1}
+            >
+              -
+            </button>
+            <div className="col-qty">{mult}</div>
 
-                        <button
-                            className='Qty__Button'
-                            onClick={() => setMult(mult + 1)}
-                        >
-                            +
-                        </button>
-                    </div>
-                </div>
-                <p
-                    onClick={() => removeItemCart(item.id)}
-                    style={{
-                        textDecoration: "underline",
-                        color: "red",
-                        cursor: "pointer",
-                    }}
-                >
-                    Remover item
-                </p>
-            </div>
-            <div className='total-parcial'>Total:{total.toFixed(2)}</div>
-            <br />
-            <hr />
-        </>
-    );
+            <button className="Qty__Button" onClick={() => setMult(mult + 1)}>
+              +
+            </button>
+          </div>
+        </div>
+        <p
+          onClick={() => removeItemCart(item.id)}
+          style={{
+            textDecoration: "underline",
+            color: "red",
+            cursor: "pointer",
+          }}
+        >
+          Remover item
+        </p>
+      </div>
+      <div className="total-parcial">Total:{total.toFixed(2)}</div>
+      <br />
+      <hr />
+    </>
+  );
 }
 export default ItemCarrinho;
--- src/context/produtosContext.jsx
+++ src/context/produtosContext.jsx
@@ -3,42 +3,42 @@ import { createContext, useState } from "react";
 export const ProductContext = createContext();
 
 function ProductContexProvider({ children }) {
-    const [selectItens, setSelectItens] = useState([]);
+  const [selectItens, setSelectItens] = useState([]);
 
-    function addItemCart(value, item) {
-        let contains = false;
+  function addItemCart(value, item) {
+    let contains = false;
 
-        if (!selectItens.length) {
-            setSelectItens((prev) => [...prev, { ...item, sizeSelect: value }]);
-            return;
-        }
-
-        selectItens.forEach((element) => {
-            if (element.id === item.id) {
-                console.log("Tem iguais");
-                alert("Produto já existe!");
-                contains = true;
-                return;
-            }
-        });
-        if (!contains)
-            setSelectItens((prev) => [...prev, { ...item, sizeSelect: value }]);
+    if (!selectItens.length) {
+      setSelectItens((prev) => [...prev, { ...item, tamanho: value }]);
+      return;
     }
 
-    function removeItemCart(item) {
-        const newSelectItens = selectItens.filter(
-            (element) => item !== element.id && element
-        );
+    selectItens.forEach((element) => {
+      if (element.id === item.id) {
+        console.log("Tem iguais");
+        alert("Produto já existe!");
+        contains = true;
+        return;
+      }
+    });
+    if (!contains)
+      setSelectItens((prev) => [...prev, { ...item, sizeSelect: value }]);
+  }
 
-        setSelectItens(newSelectItens);
-    }
-
-    return (
-        <ProductContext.Provider
-            value={{ selectItens, setSelectItens, addItemCart, removeItemCart }}
-        >
-            {children}
-        </ProductContext.Provider>
+  function removeItemCart(item) {
+    const newSelectItens = selectItens.filter(
+      (element) => item !== element.id && element
     );
+
+    setSelectItens(newSelectItens);
+  }
+
+  return (
+    <ProductContext.Provider
+      value={{ selectItens, setSelectItens, addItemCart, removeItemCart }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
 }
 export default ProductContexProvider;
